69b21d01caf3b007fa4e5b7acdd319d1efe72466
Thomas Wilburn <news@thomaswilburn.net>
2015-01-23 10:57:01 -0800

    Updated to hide map, reveal Leaflet to JS

--- root/src/js/main.js
+++ root/src/js/main.js
@@ -1,3 +1,8 @@
 //load our custom elements
 require("component-leaflet-map");
 require("component-responsive-frame");
+
+//get access to Leaflet and the map
+var element = document.querySelector("leaflet-map");
+var L = element.leaflet;
+var map = element.map;
