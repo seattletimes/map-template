b27b787f4986bd9de169a68058d8fcd7d629ef4a
sgpotts <gpotts@news-0700ng.seatimes.com>
2015-04-16 14:32:05 -0700

    Added more standard things
    Like icanhaz template for popup and css

--- root/src/js/main.js
+++ root/src/js/main.js
@@ -6,3 +6,14 @@ require("component-responsive-frame");
 var element = document.querySelector("leaflet-map");
 var L = element.leaflet;
 var map = element.map;
+
+//ICH code for popup template if needed----------
+// var ich = require("icanhaz");
+// var templateFile = require("./_popup.html");
+// ich.addTemplate("popup", templateFile);
+
+// var onEachFeature = function(feature, layer) {
+//   layer.bindPopup(ich.popup(feature.properties))
+// };
+
+ map.scrollWheelZoom.disable();
